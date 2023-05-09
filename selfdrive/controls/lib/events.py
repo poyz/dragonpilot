8ba5ce89f532d1642ce23670bf1586a4ad2361be
poyz <35866467+poyz@users.noreply.github.com>
2023-05-09 15:13:50 +1000

    Update events.py
    Take 2

--- selfdrive/controls/lib/events.py
+++ selfdrive/controls/lib/events.py
@@ -195,7 +195,7 @@ class NormalPermanentAlert(Alert):
 
 
 class StartupAlert(Alert):
-  def __init__(self, alert_text_1: str, alert_text_2: str = _("San ka punta, the the moon, road trip, broom broom"), alert_status=AlertStatus.normal):
+  def __init__(self, alert_text_1: str, alert_text_2: str = _("road trip, broom broom"), alert_status=AlertStatus.normal):
     super().__init__(alert_text_1, alert_text_2,
                      alert_status, AlertSize.mid,
                      Priority.LOWER, VisualAlert.none, AudibleAlert.none, 10.),
@@ -362,7 +362,7 @@ EVENTS: Dict[int, Dict[str, Union[Alert, AlertCallbackType]]] = {
   },
 
   EventName.startup: {
-    ET.PERMANENT: StartupAlert(_("Be ready to take over at any time"))
+    ET.PERMANENT: StartupAlert(_("San ka punta, the the moon"))
   },
 
   EventName.startupMaster: {
